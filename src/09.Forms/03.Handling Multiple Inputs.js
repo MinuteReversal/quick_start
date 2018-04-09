012612a5bf35d102245bc7d81926154a95e92a4e
MinuteReversal <114233763@qq.com>
2018-04-09 16:18:20 +0800

    form bind example

---
+++ src/09.Forms/03.Handling Multiple Inputs.js
@@ -0,0 +1,56 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : parseInt(target.value);
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    render() {
+        return (
+            <form>
+                <label>
+                    Is going:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    number of guests:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+            </form>
+        );
+    }
+}
+
+export default class Main {
+    constructor() {
+        ReactDOM.render(
+            <Reservation />,
+            document.getElementById("root")
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@
 //import Main from "./08.Lists and Keys/01"
 //import Main from "./08.Lists and Keys/02.Basic List Component"
 //import Main from "./08.Lists and Keys/03.Keys"
-import Main from "./08.Lists and Keys/04.Embedding map() in JSX"
+//import Main from "./08.Lists and Keys/04.Embedding map() in JSX"
+//import Main from "./09.Forms/01.Controlled Components"
+//import Main from "./09.Forms/02.The select Tag"
+import Main from "./09.Forms/03.Handling Multiple Inputs"
 
 new Main();
